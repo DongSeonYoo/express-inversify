13cde06aaa406e550ceb1c53a8ec9032ffbdeb76
ehdtjs0612 <inko51366@naver.com>
2023-12-09 23:57:18 +0900

    feat: config 전역변수 생성

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,6 +2,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default {
+export const ENV = {
     PORT: process.env.PORT,
+
+    DATABASE: {
+        USER: process.env.POSTGRESQL_USER,
+        HOST: process.env.POSTGRESQL_HOST,
+        DATABASE: process.env.POSTGRESQL_DATABASE,
+        PASSWORD: process.env.POSTGRESQL_PASSWORD,
+        PORT: Number(process.env.POSTGRESQL_PORT) || 5432,
+        MAX_CONNECTION: Number(process.env.POSTGRESQL_MAX_CONNECTION),
+    },
 };
