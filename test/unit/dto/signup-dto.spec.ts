a9943ae6be40de03bca99284c105844c50b5134c
ehdtjs0612 <inko51366@naver.com>
2023-12-12 16:21:35 +0900

    test: signup dto static 메서드 test

--- test/unit/dto/signup-dto.spec.ts
+++ test/unit/dto/signup-dto.spec.ts
@@ -1,60 +1,63 @@
-import { SignupDTO } from '../../../src/dtos/auth/signup.dto';
+import { SignupRequestDTO } from '../../../src/dtos/auth/signup/signup-req.dto';
+import { User } from '../../../src/entities/user.entity';
 import { BadRequestException } from '../../../src/utils/customError.util';
-import { emailRegex } from '../../../src/constants/regex.constant';
 
-describe('회원가입 dto를 검증한다', () => {
-    it('email, pw, name, major, entryYear이 있으면 에러가 발생하지 않는다', () => {
-        // given
-        const signupRequest = {
-            email: 'test123@naver.com',
-            pw: '1234..',
-            name: 'testuser',
-            major: '2',
-            entryYear: '23',
-        };
+describe('signup dto를 테스트한다', () => {
+    describe('signup dto의 static 메서드를 검증한다', () => {
+        let body;
+        beforeEach(() => {
+            body = {
+                email: 'inko51366@naver.com',
+                pw: '1234..',
+                name: 'testuser',
+                personalColor: '123123',
+                major: '2',
+                entryYear: '23',
+            };
+        });
+        it('body를 받아 회원가입 DTO를 of 메서드로 생성한다', () => {
+            // when
+            const expectedDto = SignupRequestDTO.of(body);
 
-        // when
-        const expectValue = SignupDTO.of(signupRequest);
+            // then
+            expect(expectedDto).toBeInstanceOf(SignupRequestDTO);
+        });
 
-        // then
-        expect(expectValue).toBeTruthy();
+        it('dto를 받아 toEntity 메서드로 user 엔티티로 변환해준다', () => {
+            const signupDto = SignupRequestDTO.of(body);
+
+            // when
+            const expectUserEntity = signupDto.toEntity();
+
+            // then
+            expect(expectUserEntity).toBeInstanceOf(User);
+        });
     });
 
-    it('하나라도 값이 없을 경우 400 에러를 던진다', () => {
-        // given
-        const signupRequest = {
-            // email: 'test123@naver.com',
-            pw: '1234..',
-            name: 'testuser',
-            major: '2',
-            entryYear: '23',
-        };
+    describe('dto 내부에서 validation로직을 실행한다', () => {
+        it('값이 하나라도 없으면 400에러를 던진다', () => {
+            // given
+            const body = {
+                // email: 'inko51366@naver.com',
+                pw: '1234..',
+                name: 'testuser',
+                personalColor: '123123',
+                major: '2',
+                entryYear: '23',
+            };
 
-        // when
-        const expectFunc = () => {
-            SignupDTO.of(signupRequest);
-        };
+            // when
+            const createDtoFunc = () => {
+                SignupRequestDTO.of(body);
+            };
 
-        // then
-        expect(expectFunc).toThrow(BadRequestException);
-    });
-
-    it('email 정규표현식을 지키지 않았을 시 400에러를 던진다', () => {
-        // given
-        const signupRequest = {
-            email: 'inko51366@naver.com@naver.com',
-            pw: '1234..',
-            name: 'testuser',
-            major: '2',
-            entryYear: '23',
-        };
-
-        // when
-        const expectFunc = () => {
-            SignupDTO.of(signupRequest);
-        };
-
-        // then
-        expect(expectFunc).toThrow(BadRequestException);
+            // then
+            expect(createDtoFunc).toThrow(BadRequestException);
+        });
+        it.todo('이메일 정규식을 검사한다');
+        it.todo('패스워드 정규식을 검사한다');
+        it.todo('이름 정규식을 검사한다');
+        it.todo('major가 정수인지 검사한다');
+        it.todo('entry year(입학 년도) 정규식을 검사한다');
     });
 });
