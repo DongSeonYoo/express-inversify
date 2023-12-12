f79243a3e9fb3ed080c41172ebfc47ccdb5aad58
ehdtjs0612 <inko51366@naver.com>
2023-12-12 16:20:26 +0900

    feat: try-catch 제거, fromEntity의 첫번째 매개변수로 json 전달

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -3,13 +3,9 @@ import { SignupResponseDTO } from '../dtos/auth/signup/signup-res.dto';
 import * as userRepository from '../repositories/user.repository';
 
 export const signup = async (dto: SignupRequestDTO) => {
-    try {
-        const user = dto.toEntity();
+    const user = dto.toEntity();
 
-        const result = await userRepository.signUp(user);
+    const userId = await userRepository.signUp(user);
 
-        return SignupResponseDTO.fromEntity(result, '회원가입 성공');
-    } catch (error) {
-        throw error;
-    }
+    return SignupResponseDTO.fromEntity({ userId }, '회원가입 성공');
 };
