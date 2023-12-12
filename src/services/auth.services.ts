8c833a96cffb911bb39f8d8fa32ec3eb5bddb9e5
ehdtjs0612 <inko51366@naver.com>
2023-12-12 14:24:45 +0900

    feat: service에서 dto를 받아 repository 레이어로 전달

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,9 +1,14 @@
-import { appDataSource } from '../configs/typeorm.config';
-import { SignupDTO } from '../dtos/auth/signup.dto';
-import { Belong } from '../entities/belong.entity';
+import { SignupRequestDTO } from '../dtos/auth/signup/signup-req.dto';
+import { SignupResponseDTO } from '../dtos/auth/signup/signup-res.dto';
+import * as userRepository from '../repositories/user.repository';
 
-export const signup = async (body: SignupDTO) => {
+export const signup = async (dto: SignupRequestDTO) => {
     try {
+        const user = dto.toEntity();
+
+        const result = await userRepository.signUp(user);
+
+        return SignupResponseDTO.fromEntity(result, '회원가입 성공');
     } catch (error) {
         throw error;
     }
