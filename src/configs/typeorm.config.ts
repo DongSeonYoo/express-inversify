5523c46dd0148c6b1e8688d316b121a253ab527e
ehdtjs0612 <inko51366@naver.com>
2023-12-14 23:29:16 +0900

    refactor: 와일드카드 패턴 사용

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,28 +2,6 @@ import 'reflect-metadata';
 
 import { DataSource } from 'typeorm';
 import { ENV } from './env';
-import { Belong } from '../entities/belong.entity';
-import { Club } from '../entities/club.entity';
-import { ClubBoard } from '../entities/club-board.entity';
-import { ClubPost } from '../entities/club-post.entity';
-import { ClubComment } from '../entities/club-comment.entity';
-import { User } from '../entities/user.entity';
-import { ClubMember } from '../entities/club-member.entity';
-import { Position } from '../entities/position.entity';
-import { ClubReply } from '../entities/club-reply.entity';
-import { JoinRequest } from '../entities/join-request.entity';
-import { NoticeComment } from '../entities/notice-comment.entity';
-import { PostImg } from '../entities/post-img.entity';
-import { Notice } from '../entities/notice.entity';
-import { NoticeImg } from '../entities/notice-img.entity';
-import { NoticeReply } from '../entities/notice-reply.entity';
-import { Promotion } from '../entities/promotion';
-import { PromotionComment } from '../entities/promotion-comment.entity';
-import { PromotionImg } from '../entities/promotion-img.entity';
-import { PromotionReply } from '../entities/promotion-reply.entity';
-import { Major } from '../entities/major.entity';
-import { BigCategory } from '../entities/big-category.entity';
-import { SmallCategory } from '../entities/small-category.entity';
 
 export const appDataSource = new DataSource({
     type: 'postgres',
@@ -33,31 +11,5 @@ export const appDataSource = new DataSource({
     password: ENV.database.password,
     database: ENV.database.database,
     synchronize: ENV.database.synchronize,
-    // entities: [__dirname + '/../entities/*.js'],
-    entities: [
-        Belong,
-        Club,
-        ClubBoard,
-        ClubPost,
-        ClubComment,
-        User,
-        ClubMember,
-        Position,
-        ClubReply,
-        JoinRequest,
-        NoticeComment,
-        ClubPost,
-        NoticeComment,
-        Notice,
-        NoticeImg,
-        NoticeReply,
-        Promotion,
-        PromotionComment,
-        PromotionImg,
-        PromotionReply,
-        Major,
-        PostImg,
-        BigCategory,
-        SmallCategory,
-    ],
+    entities: [__dirname + '/../entities/*'],
 });
