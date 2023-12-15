2061e648ba07d1a134aba31ba95a3404ec4d8ffb
ehdtjs0612 <inko51366@naver.com>
2023-12-16 00:50:37 +0900

    feat: 로그인 여부를 체크하는 미들웨어 작성

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from 'express';
+import { UnauthorizedException } from '../utils/customError.util';
+
+export const checkAuth: RequestHandler = (req, res, next) => {
+    try {
+        if (req.session && req.session.user) {
+            return next()
+        }
+
+        throw new UnauthorizedException('로그인 필요함미다');
+    } catch (error) {
+        return next(error);
+    }
+};
