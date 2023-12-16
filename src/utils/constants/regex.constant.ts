ce3a7614e5cd558e05be37e5ab9aa99b59e6bca3
ehdtjs0612 <inko51366@naver.com>
2023-12-17 00:16:38 +0900

    feat: regex 추가

--- src/utils/constants/regex.constant.ts
+++ src/utils/constants/regex.constant.ts
@@ -1,2 +1,9 @@
-export const emailRegex = /^0\d{10}$/;
+// account | auth
+// export const emailRegex = /^[A-Za-z0-9]{1,64}@inha\.edu$/;
+export const emailRegex = /^[A-Za-z0-9]{1,64}@inha\.edu$/;
 export const nameRegex = /^[가-힣a-zA-Z]{2,8}$/;
+export const pwRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!.]).{8,}$/;
+
+// club
+export const clubNameRegex = /^[가-힣a-zA-Z ]{1,12}$/;
+export const themeColorRegex = /^[a-zA-Z0-9]{6}$/;
