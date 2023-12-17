58222be7323e48883e868826c75c68bef6d38ac8
ehdtjs0612 <inko51366@naver.com>
2023-12-17 16:49:34 +0900

    refactor: error handling middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,11 +1,11 @@
 import { ErrorRequestHandler } from 'express';
 import { CustomError } from '../utils/customError.util';
-import { Fail, ICommonResponse } from '../utils/common/response.common';
+import { Fail } from '../utils/common/response.common';
 
 export const errorHandlingMiddleWare: ErrorRequestHandler = (error: CustomError, req, res, next) => {
     console.error(error);
     if (!error.statusCode) {
-        const serverError: ICommonResponse = {
+        const serverError: Fail = {
             message: '서버에서 오류가 발생했습니다',
             statusCode: 500,
         };
@@ -13,5 +13,5 @@ export const errorHandlingMiddleWare: ErrorRequestHandler = (error: CustomError,
         return res.status(serverError.statusCode).send(serverError);
     }
 
-    res.status(error.statusCode).send(new Fail(error.message, error.statusCode));
+    res.status(error.statusCode).send(new Fail(error.message, error.statusCode, error?.data));
 };
